711ffef4303e7c87871f88bb34d3e488f7bbe02b
Aaron Westley <aaron.westley@gmail.com>
2018-03-06 15:30:55 -0500

    Added setup.py for installing via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+    name='pylof',
+    version='1.0',
+    url='https://github.com/damjankuznar/pylof',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='damjankuznar',
+    author_email='',
+    description='Python implementation of Local Outlier Factor algorithm.'
+)
